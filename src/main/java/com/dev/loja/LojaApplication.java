aae8faeb38f305cea3a4dfc40118f97e9fd6ed38
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2022-10-20 01:41:12 -0300

    heroku depploy

--- src/main/java/com/dev/loja/LojaApplication.java
+++ src/main/java/com/dev/loja/LojaApplication.java
@@ -3,9 +3,6 @@ package com.dev.loja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dev.loja.model.Cliente;
-import com.dev.loja.service.ClienteI;
-
 
 @SpringBootApplication
 public class LojaApplication {
