9c2dbdd8b25edd92a652dc8645a5892228cd2499
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2022-10-21 18:21:16 -0300

    mensagem de usuário cadastrado renderizada na home!

--- src/main/java/com/dev/loja/controle/GUIClienteController.java
+++ src/main/java/com/dev/loja/controle/GUIClienteController.java
@@ -3,9 +3,7 @@ package com.dev.loja.controle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -31,7 +29,7 @@ public class GUIClienteController {
 	public String saveCliente(Cliente cliente, RedirectAttributes redirectAttributes) {
 		System.out.println(cliente);
 		servico.save(cliente);
-		redirectAttributes.addFlashAttribute("message", "The user has been saved sucessfully.");
+		redirectAttributes.addFlashAttribute("message", "Usuário criado com sucesso!");
 		return "redirect:/";
 	}
 
