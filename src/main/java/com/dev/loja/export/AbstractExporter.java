9b5fd01681451300775a28e9f94f18e974dd29b7
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2022-11-16 01:27:51 -0300

    correção nomenclatura de arquivo PDF (exportação de clientes)

--- src/main/java/com/dev/loja/export/AbstractExporter.java
+++ src/main/java/com/dev/loja/export/AbstractExporter.java
@@ -13,7 +13,7 @@ public class AbstractExporter {
 			String extension) throws IOException{
 		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 		String timestamp = dateFormatter.format(new Date());
-		String fileName = "users_" + timestamp + extension;
+		String fileName = "clientes_" + timestamp + extension;
 		
 		response.setContentType(contentType);
 		
